162cf5c21d58135c9f501e18b082aca5113c6602
Lionel Delamare <ldelamare@entreloups.com>
2021-11-14 23:07:07 +0100

    Creation of the Header, MovieDetails, MovieList and MovieElement components and import Bootstrap

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Header } from './Header/Header';
+export { default as MovieDetails } from './MovieDetails/MovieDetails';
+export { default as MovieList } from './MovieList/MovieList';
+export { default as MovieElement } from './MovieList/MovieElement';
